1e55e2d5d196ca9d6dd6e65c6679b62ed019e811
AlexeyBushnev <101806802+AlexeyBushnev@users.noreply.github.com>
2023-05-09 18:01:50 +0200

    Add files via upload

---
+++ test_simple_db.py
@@ -0,0 +1,26 @@
+import allure
+import pytest
+import pymssql
+
+from test_settings import SQL_SERVER_NAME
+from test_settings import USERNAME
+from test_settings import PASSWORD
+
+
+def get_db_connection_cursor2():
+    # pyodbc connection is auto closeable, no need to close manually
+    return pymssql.connect(host='localhost', server=SQL_SERVER_NAME, port='1433', user=USERNAME, password=PASSWORD)
+
+
+class TestSimpleDB:
+    @pytest.mark.test_trn_db
+    def test_simple_data_read(self):
+        allure.dynamic.title("Simple test that checks the Database is accessible and we can read the data.")
+
+        conn = get_db_connection_cursor2()
+        cursor = conn.cursor()
+        cursor.execute("""SELECT * FROM TRN.hr.jobs""")
+        rows = cursor.fetchall()
+        assert len(rows) > 0, \
+            f"""Wrong number of fields!\nExpected: Number of rows is greater 0;\nActual: {len(rows)}"""
+        
